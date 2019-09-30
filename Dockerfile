cf631c8dc69418c7d470aef5e806534846fda2c3
Vladyslav Maksymov <vmaksimov@ecisys.com>
2019-09-30 13:48:33 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.7
+
+MAINTAINER Vladyslav Maksymov (vmaksimov@ecisys.com)
+
+COPY . /opt/octopus
+
+EXPOSE 5000
+
+WORKDIR /opt/octopus
+ENV PYTHONPATH=/opt/octopus
+
+RUN pip install -r requirements.txt
+
+CMD alembic upgrade head
+CMD python octopus/__init__.py
---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from tornado.ioloop import IOLoop
+from tornado.web import Application
+
+
+def create_app():
+    return Application(
+        compress_response=True,
+    )
+
+
+if __name__ == '__main__':
+    application = create_app()
+
+    application.listen(5000)
+    IOLoop.instance().start()
